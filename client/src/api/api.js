04e8f0e6ee97fe8898bfb14aa2d36b321f9a08f4
rasool <rasooli7898>
2025-03-23 18:18:53 +0530

    last one

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://query-pzf4.onrender.com",
+  baseURL: "https://query-pzf4.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
